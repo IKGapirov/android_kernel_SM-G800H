a27d983011474d68842e82a24ee29733517c0571
ikgapirov <ikgapirov@gmail.com>
2017-02-03 12:17:59 +0500

    Add some stuff

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-if [ ! -d $(pwd)/output ]; 
-    then
-	mkdir $(pwd)/output; 
-    fi
+if [ ! -d $(pwd)/output ];  then
+    mkdir $(pwd)/output; 
+    mkdir $(pwd)/output/modules;
+fi
 
 make -C $(pwd) O=output msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig
 make -j3 -C $(pwd) O=output
 
-
-if [ ! -d $(pwd)/output/modules ]; 
-    then
-	mkdir $(pwd)/output/modules; 
-    fi
-find $(pwd)/output -name '*.ko' -exec cp -v {} $(pwd)/output/modules \;
+find $(pwd)/output -name '*.ko' -exec cp -fv {} $(pwd)/output/modules \;
 
 exit
