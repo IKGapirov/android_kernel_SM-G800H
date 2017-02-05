ab1aa0cfdcf08dc7d8b2c1e7e1a8d8b60eb3a722
alexax66 <alexax66@mail.ru>
2017-02-05 17:05:35 +0000

    Add dtbTool for building dt.img

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ fi
 
 make -C $(pwd) O=output cyanogenmod_kmini3g_defconfig
 make -j7 -C $(pwd) O=output
-cp output/arch/arm/boot/zImage output/arch/arm/boot/boot.img-kernel
 
+$(pwd)/dtbToolCM  -o $(pwd)/output/arch/arm/boot/dt.img -s 2048 -p $(pwd)/output/scripts/dtc/ $(pwd)/output/arch/arm/boot/ -v
+
+cp output/arch/arm/boot/zImage output/arch/arm/boot/boot.img-kernel
+cp output/arch/arm/boot/dt.img output/arch/arm/boot/boot.img-dt
 find $(pwd)/output -name '*.ko' -exec cp -fv {} $(pwd)/output/modules \;
 
 exit
