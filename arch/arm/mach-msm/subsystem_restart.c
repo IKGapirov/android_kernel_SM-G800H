87e16c036bf62cfed52d634388a64f3f5273a7fa
Russ Knize <Russ.Knize@motorola.com>
2017-02-07 19:14:12 +0000

    arm: mach-msm: call BUG() instead of panic()
    Change-Id: Ib205b21003870fff767f131193e6ecdd501e8454
    Signed-off-by: Russ Knize <Russ.Knize@motorola.com>
    
    Based on commit: https://github.com/hurtsky/android_kernel_motorola_msm8226/commit/52fd4b76702823a34709acbc26165f9d0106501a

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -394,7 +394,7 @@ static void do_epoch_check(struct subsys_device *dev)
 	if (time_first && n >= max_restarts_check) {
 		if ((curr_time->tv_sec - time_first->tv_sec) <
 				max_history_time_check)
-			panic("Subsystems have crashed %d times in less than "
+			PR_BUG("Subsystems have crashed %d times in less than "
 				"%ld seconds!", max_restarts_check,
 				max_history_time_check);
 	}
@@ -450,7 +450,7 @@ static void subsystem_shutdown(struct subsys_device *dev, void *data)
 
 	pr_info("[%p]: Shutting down %s\n", current, name);
 	if (dev->desc->shutdown(dev->desc) < 0)
-		panic("subsys-restart: [%p]: Failed to shutdown %s!",
+		PR_BUG("subsys-restart: [%p]: Failed to shutdown %s!",
 			current, name);
 	subsys_set_state(dev, SUBSYS_OFFLINE);
 }
@@ -474,12 +474,12 @@ static void subsystem_powerup(struct subsys_device *dev, void *data)
 	init_completion(&dev->err_ready);
         if (dev->desc->powerup(dev->desc) < 0) {
                 notify_each_subsys_device(&dev, 1, SUBSYS_POWERUP_FAILURE, NULL);
-		panic("[%p]: Powerup error: %s!", current, name);
+		PR_BUG("[%p]: Powerup error: %s!", current, name);
         }
 	ret = wait_for_err_ready(dev);
 	if (ret) {
                 notify_each_subsys_device(&dev, 1, SUBSYS_POWERUP_FAILURE,NULL);
-		panic("[%p]: Timed out waiting for error ready: %s!",
+		PR_BUG("[%p]: Timed out waiting for error ready: %s!",
 			current, name);
         }
 	subsys_set_state(dev, SUBSYS_ONLINE);
@@ -754,7 +754,7 @@ static void __subsystem_restart_dev(struct subsys_device *dev)
 			wake_lock(&dev->wake_lock);
 			queue_work(ssr_wq, &dev->work);
 		} else {
-			panic("Subsystem %s crashed during SSR!", name);
+			PR_BUG("Subsystem %s crashed during SSR!", name);
 		}
 	}
 	spin_unlock_irqrestore(&track->s_lock, flags);
@@ -823,14 +823,14 @@ int subsystem_restart_dev(struct subsys_device *dev)
 			queue_delayed_work(panic_wq, &panic_dwork, 300);
 			dump_stack();
 		} else
-			panic("%s crashed: subsys-restart: Resetting the SoC",
+			PR_BUG("%s crashed: subsys-restart: Resetting the SoC",
 				name);
 #else
-		panic("subsys-restart: Resetting the SoC - %s crashed.", name);
+		PR_BUG("subsys-restart: Resetting the SoC - %s crashed.", name);
 #endif
 		break;
 	default:
-		panic("subsys-restart: Unknown restart level!\n");
+		PR_BUG("subsys-restart: Unknown restart level!\n");
 		break;
 	}
 	module_put(dev->owner);
@@ -1324,7 +1324,7 @@ static int __init ssr_init_soc_restart_orders(void)
 
 static void panic_for_silentLog_work(struct work_struct *work)
 {
-	panic("%s crashed: subsys-restart: Resetting the SoC", subsys_name);
+	PR_BUG("%s crashed: subsys-restart: Resetting the SoC", subsys_name);
 }
 
 static int __init subsys_restart_init(void)
